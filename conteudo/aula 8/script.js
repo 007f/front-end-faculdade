9e141fca0491b46f7563b4db392ee463b0cada2a
007f <88677725+007f@users.noreply.github.com>
2024-03-27 20:24:43 -0300

    terminar-

---
+++ conteudo/aula 8/script.js
@@ -0,0 +1,16 @@
+function inner(){
+    document.getElementById('saida1').innerHTML = "<h1>InnerHTML<h1>";
+    document.getElementById('saida2').innerHTML = "";
+}
+
+function escrever(){
+    document.write("<h2>Funcao write</h2>");
+}
+
+function batata(){
+    window.alert("Mensagem popup");
+}
+
+function terminal(){
+    console.log("Resultado no terminal");
+}
